2e10997fd6bc5f1e241e09b7bf4b787c686e7c5a
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-23 07:15:12 +0900

    feat: merge in more advanced optimal rpc picker

--- static/scripts/rewards/rpc-optimization/get-optimal-provider.ts
+++ static/scripts/rewards/rpc-optimization/get-optimal-provider.ts
@@ -1,26 +1,22 @@
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { AppState } from "../app-state";
 import { getFastestRpcProvider } from "./get-fastest-rpc-provider";
 import { testRpcPerformance } from "./test-rpc-performance";
+import { AppState } from "../app-state";
 
-let optimalProvider: JsonRpcProvider;
 let isTestStarted = false;
 let isTestCompleted = false;
 
-export async function getOptimalProvider(app: AppState): Promise<JsonRpcProvider> {
-  const networkId = app.transactionNetworkId;
+export async function getOptimalProvider(app: AppState): Promise<[JsonRpcProvider, string]> {
+  const networkId = app.networkId;
+
   if (!networkId) throw new Error("Network ID not found");
 
   if (!isTestCompleted && !isTestStarted) {
     isTestStarted = true;
-    testRpcPerformance(networkId)
-      .then(() => (isTestCompleted = true))
-      .catch(console.error);
+    await testRpcPerformance(networkId).catch(console.error);
+    isTestCompleted = true;
   }
 
-  if (!optimalProvider) {
-    optimalProvider = getFastestRpcProvider(networkId);
-  }
-  app.provider = optimalProvider;
-  return optimalProvider;
+  const optimalRPC = getFastestRpcProvider(networkId);
+  return [optimalRPC, optimalRPC.connection.url];
 }
